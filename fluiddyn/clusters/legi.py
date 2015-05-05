6cd8f9fe2d759e9684b6f3bfa51a790b1b17d23c
paugier <pierre.augier@ens-lyon.org>
2015-05-05 16:33:52 +0200

    Add some Legi clusters.

--- fluiddyn/clusters/legi.py
+++ fluiddyn/clusters/legi.py
@@ -14,6 +14,19 @@ Provides:
 from fluiddyn.clusters.oar import ClusterOAR
 
 
-class Calcul8(ClusterOAR):
+class Calcul3(ClusterOAR):
     name_cluster = 'calcul3'
     nb_cores_per_node = 12
+    frontends = ['calcul8sv109', 'legilnx44']
+
+
+class Calcul9(ClusterOAR):
+    name_cluster = 'calcul9'
+    nb_cores_per_node = 8
+    frontends = ['calcul8sv109', 'legilnx44']
+
+
+class Calcul7(ClusterOAR):
+    name_cluster = 'calcul7'
+    nb_cores_per_node = 32
+    frontends = ['nrj1sv223']
