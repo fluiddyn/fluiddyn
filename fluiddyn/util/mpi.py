6fa6324b37edebc1dbbcab48308f6ae422153e1d
paugier <pierre.augier@ens-lyon.org>
2015-05-08 22:47:26 +0200

    Solve bug mpi excepthook

--- fluiddyn/util/mpi.py
+++ fluiddyn/util/mpi.py
@@ -11,8 +11,9 @@ else:
     nb_proc = comm.size
     rank = comm.Get_rank()
 
-    def my_excepthook(ex_cls, ex, tb):
-        sys.__excepthook__(ex_cls, ex, tb)
-        MPI.COMM_WORLD.Abort()
+    if nb_proc > 1:
+        def my_excepthook(ex_cls, ex, tb):
+            sys.__excepthook__(ex_cls, ex, tb)
+            MPI.COMM_WORLD.Abort()
 
-    sys.excepthook = my_excepthook
+        sys.excepthook = my_excepthook
