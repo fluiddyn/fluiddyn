469980eefd92c83cb86828bc65a221b83d5a95cd
paugier <pierre.augier@ens-lyon.org>
2016-12-05 18:32:17 +0100

    Tiny modif. rcparams.py

--- fluiddyn/output/rcparams.py
+++ fluiddyn/output/rcparams.py
@@ -9,29 +9,33 @@ Default parameters for Matplotlib figures
 import matplotlib.pyplot as plt
 
 
-def set_rcparams(fontsize=18, for_article=True, for_beamer=False):
+def set_rcparams(fontsize=18, for_article=True, for_beamer=False,
+                 fontsize_pad=9):
+
+    params = {
+        'axes.labelsize': fontsize,
+        'font.size': fontsize,
+        'legend.fontsize': fontsize,
+        'axes.titlesize': fontsize,
+        'xtick.labelsize': fontsize-2,
+        'ytick.labelsize': fontsize-2,
+        'xtick.major.pad': fontsize_pad,
+        'xtick.major.pad': fontsize_pad}
 
     if for_article or for_beamer:
-        params = {
-            # 'backend': 'ps',
-            'axes.labelsize': fontsize,
-            'font.size': fontsize,
-            'legend.fontsize': fontsize,
-            'axes.titlesize': fontsize,
-            'xtick.labelsize': fontsize,
-            'ytick.labelsize': fontsize,
-            'xtick.major.pad': 9,
-            'xtick.major.pad': 9,
+        params_tmp = {
             'text.usetex': True,
             'font.family': 'serif',
             'font.serif': 'Computer Modern Roman',
             'font.sans-serif': 'Computer Modern Roman',
             'ps.usedistiller': 'xpdf'}
 
+        for k, v in params_tmp.items():
+            params[k] = v
+
     if for_beamer:
         params['font.family'] = 'sans-serif'
         preamble = r'''\usepackage[cm]{sfmath}'''
         plt.rc('text.latex', preamble=preamble)
 
-    if for_article or for_beamer:
-        plt.rcParams.update(params)
+    plt.rcParams.update(params)
