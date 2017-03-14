6561daa1d5821982da01f28c773d3297c3e8d952
paugier <pierre.augier@ens-lyon.org>
2017-03-14 22:04:42 +0100

    Improve multitiff test.

--- fluiddyn/io/test/test_multitiff.py
+++ fluiddyn/io/test/test_multitiff.py
@@ -6,8 +6,7 @@ Test multitiff module
 
 import unittest
 import os
-from glob import glob
-from shutil import rmtree, copy
+from shutil import rmtree
 
 import numpy as np
 from PIL import Image
@@ -19,32 +18,37 @@ from ..multitiff import (
     reorganize_single_frame_2Dpiv_data,
     reorganize_double_frame_2Dpiv_data)
 
+from ..redirect_stdout import stdout_redirected
+
 
 class TestMultiTIFF(unittest.TestCase):
     """Test fluiddyn.io.multitiff module."""
-    def setUp(self):
+
+    @classmethod
+    def setUpClass(cls):
         def im(maxval, dtype, shape=(10, 10)):
             """Generate a test image."""
             return (maxval * np.random.random(shape)).astype(dtype)
 
-        self._work_dir = 'test_fluiddyn_io_multitiff'
-        if not os.path.exists(self._work_dir):
-            os.mkdir(self._work_dir)
+        cls._work_dir = 'test_fluiddyn_io_multitiff'
+        if not os.path.exists(cls._work_dir):
+            os.mkdir(cls._work_dir)
 
-        # we have to be able to produce a small multiframe tiff file.
+        os.chdir(cls._work_dir)
+        cls.n_frames = 4
+        cls.nb_files = 2
 
-        os.chdir(self._work_dir)
-        self.n_frames = 5
-        self.path = 'test.tif'
-        arrays = [im(2 ** 8 - 1, np.int8) for i in range(self.n_frames)]
-        imsave(self.path, arrays, as_int=True)
+        for ifile in range(cls.nb_files):
+            arrays = [im(2 ** 8 - 1, np.int8) for i in range(cls.n_frames)]
+            imsave('test{}.tif'.format(ifile), arrays, as_int=True)
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(cls):
         os.chdir('..')
-        rmtree(self._work_dir)
+        rmtree(cls._work_dir)
 
     def test_imsave(self):
-        im = Image.open(self.path)
+        im = Image.open('test0.tif')
         im.load()
         n_frames = im.n_frames
         im.close()
@@ -52,6 +56,26 @@ class TestMultiTIFF(unittest.TestCase):
             raise ValueError(
                 'Multiframe TIFF imsave unsuccessful. No. of frames={}'.format(
                     n_frames))
+
+    def test_single_frame_3Dscannedpiv(self):
+        with stdout_redirected():
+            reorganize_single_frame_3Dscannedpiv_data(
+                'test*.tif', 2, outputdir='.', outputext='png', erase=True)
+
+    def test_double_frame_3Dscannedpiv(self):
+        with stdout_redirected():
+            reorganize_double_frame_3Dscannedpiv_data(
+                'test*.tif', 2, outputdir='.', outputext='png', erase=False)
+
+    def test_single_frame_2Dpiv(self):
+        with stdout_redirected():
+            reorganize_single_frame_2Dpiv_data(
+                'test*.tif', outputdir='.', outputext='png', erase=False)
+
+    def test_double_frame_2Dpiv(self):
+        with stdout_redirected():
+            reorganize_double_frame_2Dpiv_data(
+                'test*.tif', outputdir='.', outputext='png', erase=False)
 
 
 if __name__ == '__main__':
