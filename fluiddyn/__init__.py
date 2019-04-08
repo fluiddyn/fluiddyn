ea56e6a80e491aad15cad5bd954a35685ccc78a3
paugier <pierre.augier@univ-grenoble-alpes.fr>
2019-04-08 13:58:30 +0200

    Add __citation__ (see https://github.com/adrn/CitationPEP)

--- fluiddyn/__init__.py
+++ fluiddyn/__init__.py
@@ -2,7 +2,7 @@
 FluidDyn
 ========
 
-Provides 
+Provides
 
   1. An object-oriented and modular set of solvers.
   2. Tools useful for carrying out experiments in fluid dynamics.
@@ -27,6 +27,20 @@ from .util.util import (
 from .util import constants
 from ._version import __version__
 
+__citation__ = """
+@article{fluiddyn,
+doi = {10.5334/jors.237},
+year = {2019},
+publisher = {Ubiquity Press,  Ltd.},
+volume = {7},
+author = {Pierre Augier and Ashwin Vishnu Mohanan and Cyrille Bonamy},
+title = {{FluidDyn}: A Python Open-Source Framework for Research and Teaching in Fluid Dynamics
+    by Simulations,  Experiments and Data Processing},
+journal = {Journal of Open Research Software}
+}
+"""
+
+
 __all__ = [
     "__version__",
     "constants",
@@ -34,4 +48,5 @@ __all__ = [
     "time_as_str",
     "get_memory_usage",
     "ipydebug",
+    "__citation__",
 ]
