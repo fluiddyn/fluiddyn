5e00527ad245671d2c1fb4f2950d1371969e6088
Ashwin Vishnu <avmo@kth.se>
2020-10-03 22:09:16 +0200

    Add links on the sidebar
    Original implementation by @ibLeDy
    See: https://github.com/poliastro/poliastro/pull/1023

--- doc/conf.py
+++ doc/conf.py
@@ -148,6 +148,42 @@ html_theme = "sphinx_rtd_theme"
 # documentation.
 # html_theme_options = {}
 
+# Values to pass into the template engine's context for all pages.
+html_context = {
+    "sidebar_external_links_caption": "Links",
+    "sidebar_external_links": [
+        (
+            '<i class="fa fa-cube fa-fw"></i> PyPI',
+            f"https://pypi.org/project/{project.lower()}",
+        ),
+        (
+            '<i class="fa fa-cube fa-fw"></i> Conda forge',
+            f"https://anaconda.org/conda-forge/{project.lower()}",
+        ),
+        (
+            '<i class="fa fa-code fa-fw"></i> Source code',
+            f"https://foss.heptapod.net/fluiddyn/{project.lower()}",
+        ),
+        (
+            '<i class="fa fa-bug fa-fw"></i> Issue tracker',
+            f"https://foss.heptapod.net/fluiddyn/{project.lower()}/-/issues",
+        ),
+        #  ('<i class="fa fa-rss fa-fw"></i> Blog', 'https://...'),
+        (
+            '<i class="fa fa-comments fa-fw"></i> Chat',
+            "https://matrix.to/#/#fluiddyn-users:matrix.org",
+        ),
+        (
+            '<i class="fa fa-envelope fa-fw"></i> Mailing list',
+            "https://www.freelists.org/list/fluiddyn",
+        ),
+        (
+            '<i class="fa fa-file-text fa-fw"></i> Citation',
+            "https://doi.org/10.5334/jors.237",
+        ),
+    ],
+}
+
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
 
