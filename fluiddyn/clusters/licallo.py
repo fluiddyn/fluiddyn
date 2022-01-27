bf6b02c6e79507774c0a2da362383a8a80f6a16a
vlabarre <vincent.labarre@oca.eu>
2022-01-27 19:18:45 +0100

    New environment

--- fluiddyn/clusters/licallo.py
+++ fluiddyn/clusters/licallo.py
@@ -25,6 +25,7 @@ class Licallo(ClusterSlurm):
             "#SBATCH -p x40",
             "OPT=$HOME/opt",
             "module purge",
+            "module load intel-gnu8-runtime/19.1.2.254 phdf5/1.12.0 impi/2019.8.254 fftw3/3.3.8",
             "export LANG=C",
             "export LC_LANG=C",
             "cd ${SLURM_SUBMIT_DIR}",
