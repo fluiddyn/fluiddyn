cbcb195dbe347ea35dc9dae86fdfdb46fa15deb0
paugier <pierre.augier@univ-grenoble-alpes.fr>
2024-01-18 12:48:35 +0100

    Add few myst-nb options

--- doc/conf.py
+++ doc/conf.py
@@ -351,6 +351,10 @@ autodoc_member_order = "bysource"
 
 todo_include_todos = True
 
+nb_execution_raise_on_error = True
+nb_execution_show_tb = True
+nb_merge_streams = True
+
 myst_enable_extensions = [
     "amsmath",
     # "attrs_inline",
