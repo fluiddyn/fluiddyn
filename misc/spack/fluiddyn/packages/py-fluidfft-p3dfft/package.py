14d7bf852c26fba66e462a2e4dbf104de505cee7
paugier <pierre.augier@univ-grenoble-alpes.fr>
2024-08-22 08:26:18 +0200

    py-fluidfft-p3dfft

--- misc/spack/fluiddyn/packages/py-fluidfft-p3dfft/package.py
+++ misc/spack/fluiddyn/packages/py-fluidfft-p3dfft/package.py
@@ -16,23 +16,23 @@ from spack.package import (
 )
 
 
-class PyFluidfftPfft(PythonPackage):
-    """Fluidfft MPI plugin using pfft."""
+class PyFluidfftP3dfft(PythonPackage):
+    """Fluidfft MPI plugin using p3dfft."""
 
-    pypi = "fluidfft-pfft/fluidfft_pfft-0.0.1.tar.gz"
+    pypi = "fluidfft-p3dfft/fluidfft_p3dfft-0.0.1.tar.gz"
 
     maintainers("paugier")
     license("CECILL-B", checked_by="paugier")
 
     # fmt: off
-    version("0.0.1", sha256="ef8255bd78c9d2dbfb11715542b221d457eedfa0a5b0bbdd1b95e8fbe64043c5")
+    version("0.0.1", sha256="1c291236a43045b9f8b9725e568277c5f405d2e2d9f811ba1bc9c5e1d9f2f827")
     # fmt: on
 
     with default_args(type=("build", "run")):
         extends("python@3.9:")
         depends_on("py-mpi4py")
 
-    depends_on("pfft", type="link")
+    depends_on("p3dfft3", type="link")
 
     with default_args(type="build"):
         # Required to use --config-settings
