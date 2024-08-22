405f2bd9bdbdbd61ae2d461c12f65a4f3fe9e7be
paugier <pierre.augier@univ-grenoble-alpes.fr>
2024-08-22 15:26:19 +0200

    Spack fluidfft 0.4.2

--- misc/spack/fluiddyn/packages/py-fluidfft/package.py
+++ misc/spack/fluiddyn/packages/py-fluidfft/package.py
@@ -20,13 +20,14 @@ from spack.package import (
 class PyFluidfft(PythonPackage):
     """Efficient and easy Fast Fourier Transform (FFT) for Python."""
 
-    pypi = "fluidfft/fluidfft-0.4.1.tar.gz"
+    pypi = "fluidfft/fluidfft-0.4.2.tar.gz"
 
     maintainers("paugier")
 
     license("CECILL-B", checked_by="paugier")
 
     # fmt: off
+    version("0.4.2", sha256="5e35f1fb647da2fa65c116bb0d598fc9cb975cd95c41022644c27dc726c36073")
     version("0.4.1", sha256="b17e64c7b2be47c61d6ac7b713e0e8992cf900d2367381288c93a56090e6c0c1")
     version("0.4.0.post1", sha256="70791c92f43d7611c5db89d069e745875fca9be02948156a6c1b184fbc87cb4d")
     version("0.3.5", sha256="56ba7213eb79af6afeb60fb19eb4a31b9f4d64f295683f9743790c71677ead87")
